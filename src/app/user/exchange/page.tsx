16ee13d07443b7c4cd4a40a6d96a22c3df17248d
Satya Sandeep <satyasandeep786@gmail.com>
2024-08-17 21:58:24 +0530

    feat: Exchange

--- src/app/user/exchange/page.tsx
+++ src/app/user/exchange/page.tsx
@@ -1,39 +1,35 @@
 "use client";
 
-import React from "react";
-import { useWeb3Modal } from "@web3modal/wagmi/react";
-import { AnimatedButton } from "@/components/Buttons";
-
 const Exchange = () => {
-  const { open } = useWeb3Modal();
-
-  // Handle potential errors
-  const handleOpenModal = () => {
-    try {
-      open({ view: "Account" });
-    } catch (error) {
-      console.error("Error opening Web3Modal:", error);
-      // You can show a user-friendly message here
-    }
-  };
-
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-semibold mb-6">Crypto Exchange</h1>
-      <div className="bg-white p-6 shadow-lg rounded-lg max-w-md mx-auto">
+    <div className="flex flex-col md:flex-row gap-6 p-6">
+      {/* Left Side: Information about Onramp */}
+      <div className="flex-1 bg-white p-6 shadow-xl rounded-lg max-w-md mx-auto  bg-gradient-to-r from-blue-400 to-purple-500 ">
+        <h1 className="text-4xl font-bold mb-4 text-gray-800">
+          What is Onramp?
+        </h1>
+        <p className="text-lg text-gray-600 mb-6">
+          Onramp allows you to easily convert fiat currency into cryptocurrency
+          directly within the app. With Onramp, you can quickly buy popular
+          digital assets and get started with your crypto journey seamlessly.
+          Enjoy a smooth and secure onboarding process without the hassle.
+        </p>
+        <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+          Supported Assets
+        </h2>
+        <ul className="list-disc list-inside text-gray-600">
+          <li>Bitcoin (BTC)</li>
+          <li>Ethereum (ETH)</li>
+          <li>USD Coin (USDC)</li>
+          <li>Other popular cryptocurrencies</li>
+        </ul>
+      </div>
+
+      {/* Right Side: Onramp Widget and Swap Button */}
+      <div className="flex-1 bg-white p-6 shadow-xl rounded-lg max-w-md mx-auto">
+        <h1 className="text-2xl font-semibold mb-6 text-gray-800">On Ramp</h1>
         <w3m-onramp-widget aria-label="Onramp Widget" />
-        <AnimatedButton
-          className="bg-black text-white flex justify-center group relative mt-4"
-          onClick={handleOpenModal}
-          aria-label="Open Swap Modal"
-        >
-          <span className="group-hover:translate-x-40 text-center transition duration-500">
-            Swap
-          </span>
-          <div className="-translate-x-40 group-hover:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
-            ⇄
-          </div>
-        </AnimatedButton>
+        <span className="text-sm"> Powered By Coinbase</span>
       </div>
     </div>
   );
