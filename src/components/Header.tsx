18198f086279a2a64453a0f7ec67b90e47a1067e
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2024-08-17 21:31:02 +0530

    fix: Update logo in dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,8 +10,8 @@ const DashboardHeader: React.FC = () => {
     <header className="w-full py-4 px-8 bg-[#FCFAF6] dark:bg-gray-900 flex justify-between items-center ">
       <div className="flex items-center">
         <p className="text-[26px] font-bold text-left uppercase">
-          <span className="text-[#222]">Bit</span>
-          <span className="text-[#4eb680]">core</span>
+          <span className="text-[#222] dark:text-white">Safe</span>
+          <span className="text-[#4eb680]">Connect</span>
         </p>
       </div>
       <div className="flex items-center gap-x-4">
